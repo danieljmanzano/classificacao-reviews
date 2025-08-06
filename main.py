d48fb78ff2f128837e8db4641350d7259d3f7feb
Daniel Jorge Manzano <djorgemanzano@gmail.com>
2025-08-06 09:22:44 -0300

    oi

---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+ideia de projeto de tratamento de dados e machine learning
+
+classifica reviews de produtos em positivas e negativas
+dataset tirado de: https://www.kaggle.com/datasets/snap/amazon-fine-food-reviews
+'''
+
